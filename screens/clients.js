7b5e81c7c7331d5ce233220be037738b4bead361
Julio Pineda Hernandez <julio-hdz05@hotmail.com>
2021-04-26 11:00:13 -0500

    Clients on the view from firebase

--- screens/clients.js
+++ screens/clients.js
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
+import { getClients } from '../utils/actions'
+import ListClients from '../clientes/ListClients'
+import { size } from 'lodash'
+import navigation from '../navigation/Navigation'
+
 
 export default function clients() {
+    const [startClient, setStartClient] = useState(null)
+    const [clients, setClients] = useState([])
+
+    const limitClients = 
+    useFocusEffect(
+        useCallback(async() => {
+                const answer = await getClients(limitClients)
+                if(answer.statusResponse) {
+                    setStartClient(answer.startClients)
+                    setClients(answer.clientes)
+                }
+            },
+            [],
+        )
+    )
+
     return (
         <View>
-            <Text>clients</Text>
+            {
+                size(clients) > 0 ? (
+                    <ListClients
+                    clientes={clients}
+                    navigation={navigation}
+                    />
+                ) : (
+                    <View style={styles.notFoundView}>
+                        <Text>No hay na'</Text>
+                    </View>
+                )
+            }
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    notFoundView: {
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
